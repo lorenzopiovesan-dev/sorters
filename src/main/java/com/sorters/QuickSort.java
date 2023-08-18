6925d1a5017804e6787029acd92cbf32614b2875
Lorenzo Piovesan <lorenzo.piovesan@protonmail.com>
2023-08-18 22:25:37 +0200

    feature : return empty list if empty list is provided

--- src/main/java/com/sorters/QuickSort.java
+++ src/main/java/com/sorters/QuickSort.java
@@ -1,5 +1,6 @@
 package com.sorters;
 
+import java.util.Collections;
 import java.util.List;
 
 public class QuickSort {
@@ -7,6 +8,9 @@ public class QuickSort {
     public List<Integer> sort(List<Integer> numList) {
         if (numList == null) {
             throw new RuntimeException("Null list not allowed");
+        }
+        if (numList.isEmpty()) {
+            return Collections.emptyList();
         }
         return null;
     }
--- src/test/java/com/sorters/QuickSortTest.java
+++ src/test/java/com/sorters/QuickSortTest.java
@@ -4,6 +4,8 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
 public class QuickSortTest {
 
     @Test
@@ -17,6 +19,20 @@ public class QuickSortTest {
 
         // Assert
         Assertions.assertEquals("Null list not allowed", sorted.getMessage());
+    }
+
+    @Test
+    @DisplayName("Provided an empty list of numbers, an empty list is returned")
+    public void sort_test_2() {
+        // Arrange
+        final List<Integer> numList = List.of();
+        final var quicksort = new QuickSort();
+
+        // Act
+        final var sorted = quicksort.sort(numList);
+
+        // Assert
+        Assertions.assertTrue(sorted.isEmpty());
     }
 
 }
